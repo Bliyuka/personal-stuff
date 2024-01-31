8b0605908910288b760ecfc8ea313f3cbe438d5a
Nguyễn Minh Tuấn <minhtuann903@gmail.com>
2024-01-31 15:41:47 +0700

    improve some object

--- Python/buckshot roulette/v1 but def.py
+++ Python/buckshot roulette/v1 but def.py
@@ -2,48 +2,81 @@ import random
 import time
 
 
-# class shotgun():
-#     def __init__(self):
-#         self.totShot = random.randint(2,6)
-#         self.killShot = random.randint(1,self.totShot-1)
-#         self.blankShot = self.totShot - self.killShot
+class shotgun():
+    def __init__(self):
+        self.totShot = random.randint(2,6)
+        self.killShot = random.randint(1,self.totShot-1)
+        self.blankShot = self.totShot - self.killShot
+        # self.Chamber = random.shuffle(["KILLSHOT"] * self.killShot + ["BLANKSHOT"] * self.blankShot)
+        
+    def getTotalShot(self):
+        return self.totShot
+    
+    def getKillShot(self):
+        return self.killShot
+    
+    def getBlankShot(self):
+        return self.blankShot
+    
+    def getChamber(self):
+        list = ["KILLSHOT"] * self.killShot + ["BLANKSHOT"] * self.blankShot
+        random.shuffle(list)
+        return list
+    
+    def reload(self):
+        time.sleep(0.5)
+        print("\n\nOut of ammo")
+        time.sleep(0.5)
+        print("\nR E L O A D")
+        time.sleep(0.5)
+        print(f"\nThere are {gun.getTotalShot()} shots: {gun.getKillShot()} deadly shot(s) and {gun.getBlankShot()} blank shot(s)")
 
-#     def currentChamber(self):
-#         Chamber = ["KILLSHOT"] * self.killShot + ["BLANKSHOT"] * self.blankShot
-#         random.shuffle(Chamber)
-#         return Chamber
 
-totalShot = random.randint(2,6)
-killShot = random.randint(1,totalShot-1)
-blankShot = totalShot - killShot
+# totalShot = random.randint(2,6)
+# killShot = random.randint(1,totalShot-1)
+# blankShot = totalShot - killShot
 
-currentMag = ["KILLSHOT"] * killShot + ["BLANKSHOT"] * blankShot
-random.shuffle(currentMag)
+# currentMag = ["KILLSHOT"] * killShot + ["BLANKSHOT"] * blankShot
+# random.shuffle(currentMag)
 
 
 class Player():
     def shootOpponent(self,opponent_hp, bullet):
         global currentBulletPosition
-        print("*clack*\n*clack*")
+        print()
+        time.sleep(0.5)
+        print("*c l a c k*")
+        time.sleep(0.5)
+        print("*c l a c k*")
+        time.sleep(1)
         if bullet == "KILLSHOT":
-            print("BANG!!")
+            print("\nBANG!!")
             opponent_hp -= 1
         else:
-            print("*P L O P*")
-            print("Nothing happened")
+            print("\n*P L O P*")
+            time.sleep(0.5)
+            print("\nNothing happened")
         currentBulletPosition += 1
+        time.sleep(1)
         return opponent_hp
 
     def shootYourself(self,hp, bullet):
         global currentBulletPosition
-        print("*clack*\n*clack*")
+        print()
+        time.sleep(0.5)
+        print("*c l a c k*")
+        time.sleep(0.5)
+        print("*c l a c k*")
+        time.sleep(1)
         if bullet == "KILLSHOT":
-            print("BANG!!")
+            print("\nBANG!!")
             hp -= 1
         else:
-            print("*P L O P*")
-            print("Nothing happened")
+            print("\n*P L O P*")
+            time.sleep(0.5)
+            print("\nNothing happened")
         currentBulletPosition += 1
+        time.sleep(1)
         return hp
 
 
@@ -53,8 +86,7 @@ class Player():
         user = int(input("Make your move: "))
         return user
 
-
-def player1Move(player, next_bullet):
+def player1Move(player, next_bullet,a,currentBulletPosition):
     global again,p1_health, p2_health, playerTurn
     print("\nPLAYER 1 move:")
     decide = player.decision()
@@ -66,10 +98,10 @@ def player1Move(player, next_bullet):
         p1_health = player.shootYourself(p1_health, next_bullet)
     print(f"\nplayer 1 hp: {p1_health}")
     print(f"player 2 hp: {p2_health}")
+    print(f"Bullet Remaining: {a - currentBulletPosition -1}")
     return
 
-
-def player2Move(player, next_bullet):
+def player2Move(player, next_bullet, a, currentBulletPosition):
     global again, p1_health, p2_health, playerTurn
     print("\nPLAYER 2 move:")
     decide = player.decision()
@@ -81,52 +113,53 @@ def player2Move(player, next_bullet):
         p2_health = player.shootYourself(p2_health,next_bullet)
     print(f"\nplayer 1 hp: {p1_health}")
     print(f"player 2 hp: {p2_health}")
+    print(f"Bullet Remaining: {a - currentBulletPosition -1}")
     return
 
-# actual game here
+
+# -------------------------------------------actual game here-------------------------------------------
+
+gun = shotgun()
+currentChamber = gun.getChamber()
 currentBulletPosition = 0
-print(f"\neach player has {round(totalShot/2)} hp")
+
+
+print(f"\neach player has {round(gun.getTotalShot()/2)} hp")
 print("whoever out of lives first loses")
-p1_health = round(totalShot/2)
+
+p1_health = round(gun.getTotalShot()/2)
 p2_health = p1_health
 
-print(f"\nThere are {totalShot} shots: {killShot} deadly shot(s) and {blankShot} blank shot(s)")
+# p1_health = 10
+# p2_health = p1_health
+
+print(f"\nThere are {gun.getTotalShot()} shots: {gun.getKillShot()} deadly shot(s) and {gun.getBlankShot()} blank shot(s)")
 
 p1 = Player()
 p2 = Player()
 
 playerTurn = 1
 
-again = True
 while p1_health > 0 and p2_health > 0:
-    while currentBulletPosition <= len(currentMag)-1 and p1_health > 0 and p2_health > 0:
-        while again and p1_health > 0 and playerTurn == 1:
-            player1Move(p1, currentMag [currentBulletPosition])
-            print(f"Bullet Remaining: {len(currentMag) - currentBulletPosition}")
+    a = len(currentChamber)
+    while currentBulletPosition <= len(currentChamber)-1 and p1_health > 0 and p2_health > 0:
+        while p1_health > 0 and playerTurn == 1:
+            player1Move(p1, currentChamber[currentBulletPosition],a,currentBulletPosition)
+            
 
-        if currentBulletPosition > len(currentMag)-1:
+        if currentBulletPosition > len(currentChamber)-1:
             break
-        while not again and p1_health > 0 and p2_health > 0 and playerTurn == 2:
-            if currentBulletPosition > len(currentMag)-1:
+        while p1_health > 0 and p2_health > 0 and playerTurn == 2:
+            if currentBulletPosition > len(currentChamber)-1:
                 break
             else:
-                player2Move(p2, currentMag[currentBulletPosition])
-                print(f"Bullet Remaining: {len(currentMag) - currentBulletPosition}")
+                player2Move(p2, currentChamber[currentBulletPosition],a,currentBulletPosition)
 
-    if currentBulletPosition >= len(currentMag)-1:
-        again = True
+    if currentBulletPosition > len(currentChamber)-1 and p1_health > 0 and p2_health > 0:
         currentBulletPosition = 0
-        totalShot = random.randint(2,6)
-        killShot = random.randint(1,totalShot-1)
-        blankShot = totalShot - killShot
-
-        currentMag = ["KILLSHOT"] * killShot + ["BLANKSHOT"] * blankShot
-        random.shuffle(currentMag)
-
-        print("\n\nOut of ammo")
-        print("R E L O A D")
-        print(f"\nThere are {totalShot} shots: {killShot} deadly shot(s) and {blankShot} blank shot(s)")
-
+        gun = shotgun()
+        currentChamber = gun.getChamber()
+        gun.reload()
     else:
         break
-print("We have a loser")
+print("\nWe have a loser")
