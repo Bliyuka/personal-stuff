d34ee8658570045406c94a033bcba109523b8076
Nguyễn Minh Tuấn <minhtuann903@gmail.com>
2024-02-12 17:30:00 +0700

    now player can use items

--- Python/buckshot roulette/v4.py
+++ Python/buckshot roulette/v4.py
@@ -8,6 +8,7 @@ class shotgun():
         self.killShot = random.randint(1,self.totShot-1)
         self.blankShot = self.totShot - self.killShot
         self.Chamber = ["KILLSHOT"] * self.killShot + ["BLANKSHOT"] * self.blankShot
+        self.sawed = False
 
         
     def getTotalShot(self):
@@ -41,23 +42,29 @@ class shotgun():
 
 
 class Player():
-    def __init__(self, health):
+    def __init__(self, health, name):
+        self.name = name
         self.hp = health
         self.Inventory = []
+        self.isHandcuff = False
 
     def getHP(self):
         return self.hp
 
 
-    def playerGetShot(self):
-        self.hp = self.hp - 1
+    def playerGetShot(self, gun):
+        if gun.sawed == False:
+            self.hp = self.hp - 1
+        else:
+            self.hp = self.hp - 2
+            gun.sawed = False
 
 
     def decision(self):
         sleep(1)
-        print("1) Shoot opponent")
+        print("\n1) Shoot opponent")
         print("2) Shoot yourself")
-        print("\n---INVENTORY---")
+        print("-INVENTORY-")
         self.showInv()
         print()
         user = input("Make your move: ")
@@ -65,7 +72,7 @@ class Player():
         return user
 
 
-    def shootOpponent(self,player, bullet):
+    def shootOpponent(self,player, bullet, gun):
         global currentBulletPosition
         print()
         sleep(0.5)
@@ -75,16 +82,17 @@ class Player():
         sleep(1)
         if bullet == "KILLSHOT":
             print("\nB A N G ! !")
-            player.playerGetShot()
+            player.playerGetShot(gun)
         else:
             print("\n*P L O P*")
             sleep(0.5)
             print("\nNothing happened")
         currentBulletPosition += 1
         sleep(1)
+        showHealthBullet(p1, p2, gun.getTotalShot(), currentBulletPosition)
 
 
-    def shootYourself(self, player, bullet):
+    def shootYourself(self, bullet, gun):
         global currentBulletPosition
         print()
         sleep(0.5)
@@ -94,66 +102,70 @@ class Player():
         sleep(1)
         if bullet == "KILLSHOT":
             print("\nB A N G ! !")
-            player.playerGetShot()
+            self.playerGetShot(gun)
         else:
             print("\n*P L O P*")
             sleep(0.5)
             print("\nNothing happened")
         currentBulletPosition += 1
         sleep(1)
+        showHealthBullet(p1, p2, gun.getTotalShot(), currentBulletPosition)
 
-    def playerMove(self, nextBullet, opponent, switchTurn):
+    def playerMove(self, nextBullet, opponent, switchTurn, gun):
         global playerTurn
-        while True:
+        if not self.isHandcuff:
+            
             decide = self.decision()
             if decide == "1":
-                self.shootOpponent(opponent, nextBullet)
+                self.shootOpponent(opponent, nextBullet, gun)
                 playerTurn = switchTurn
-                break
+                
             elif decide == "2":
-                self.shootYourself(self, nextBullet)
-                
-            # ---------- ITEM PART------------
-            elif decide == "handcuff" or decide == "hand-cuff" or decide == "hand cuff" and self.itemAvailable("Hand-Cuff"):
-                    print("handcuff test\n")
-            elif decide == "beer" and self.itemAvailable("Beer"):
-                
-                    print("beer test\n")
-            elif decide == "mg" or decide == "magnifying glass" or decide == "glass" and self.itemAvailable("Magnifying Glass"):
-                
-                    print("glass test\n")
-            elif decide == "saw" and self.itemAvailable("Saw"):
-                
-                    print("saw test\n")
-            elif decide == "cigar" or decide == "cig" and self.itemAvailable("Cigar"):
-                
-                    print("cigar test\n")
-            else:
-                print("Enter a valid option!\n")
+                self.shootYourself(nextBullet, gun)
+                # ---------- ITEM PART------------
+            else: 
+                self.useItem(decide, opponent)
+        else:
+            print(f"{self.name} breaks out of the hand-cuff")
+            sleep(0.5)
+            self.isHandcuff = False
+            playerTurn = switchTurn
+        
         return
 
 # ------------------------------------------------BOT SECTION------------------------------------------------------------
     def botMove(self, nextBullet, opponent, switchTurn, gun):
         global playerTurn
-        print("Bot's Inventory")
-        self.showInv()
-        print()
         list = gun.getChamber()
-        if list[-1] == "KILLSHOT":
-            decide = 1
-        elif list[-1] == "BLANKSHOT":
-            decide = 2
+        if not self.isHandcuff:
+            print("Bot's Inventory")
+            self.showInv()
+            print()
+            if list[-1] == "KILLSHOT":
+                decide = 1
+            elif list[-1] == "BLANKSHOT":
+                decide = 2
+            else:
+                decide = self.coinFlip()
+            sleep(1)
+            if decide == 1:
+                print("Bot chose to shoot player")
+                self.shootOpponent(opponent, nextBullet, gun)
+                playerTurn = switchTurn
+            else:
+                print("Bot chose to shoot himself")
+                self.shootYourself(nextBullet, gun)
         else:
-            decide = self.coinFlip()
-        sleep(1)
-        if decide == 1:
-            print("Bot chose to shoot player")
-            self.shootOpponent(opponent, nextBullet)
+            print(f"{self.name} breaks out of the hand-cuff")
+            sleep(0.5)
+            self.isHandcuff = False
             playerTurn = switchTurn
-        else:
-            print("Bot chose to shoot himself")
-            self.shootYourself(self, nextBullet)
+
         return
+
+    # def botUseItem(self):
+        # for i in self.Inventory:
+            
 
     def coinFlip(self):
         return random.randint(1,2)
@@ -195,21 +207,79 @@ class Player():
             if item in self.Inventory:
                 return True
             else:
-                print("you dont have this!\n")
+                print("\nyou dont have this!\n")
                 return False
 
+    def useItem(self, decide, opponent):
+        if decide == "handcuff" or decide == "hand-cuff" or decide == "hand cuff" and self.itemAvailable("Hand-Cuff"):
+            try: self.Inventory.remove("Hand-Cuff")
+            except:
+                sleep(1)
+                print("Item not available")
+            else:
+                self.useHandcuff(opponent)
+        elif decide == "beer" and self.itemAvailable("Beer"):
+                try: self.Inventory.remove("Beer") 
+                except:
+                    sleep(1)
+                    print("Item not available")
+                else:
+                    self.useBeer(gun)
+        elif decide == "mg" or decide == "magnifying glass" or decide == "glass" and self.itemAvailable("Magnifying Glass"):
+                try: self.Inventory.remove("Magnifying Glass")
+                except:
+                    sleep(1)
+                    print("Item not available")
+                else:
+                    next_bullet = self.useMG(gun)
+                    print(f"\nThe next bullet in the gun is a {next_bullet}\n")
+        elif decide == "saw" and self.itemAvailable("Saw"):
+                try: self.Inventory.remove("Saw")
+                except:
+                    sleep(1)
+                    print("Item not available")
+                else:
+                    self.useSaw(gun)
+        elif decide == "cigar" or decide == "cig" and self.itemAvailable("Cigar"):
+                try: self.Inventory.remove("Cigar")
+                except:
+                    sleep(1)
+                    print("Item not available")
+                else:
+                    self.useCigar()
+        else: 
+            print("Enter a valid option\n")
 
 # HAND-CUFF
-    # def useHandcuff(self):
-        
+    def useHandcuff(self, opponent):
+        opponent.isHandcuff = True
+        print(f"\n{opponent.name} got hand-cuffed\n")
+        sleep(1)
+
 # BEER
-
+    def useBeer(self, gun):
+        global currentBulletPosition
+        sleep(0.5)
+        print(f"\n{self.name} drank a beer and pop out a gun shell")
+        print(f"It was a {gun.getChamber()[currentBulletPosition]}\n")
+        currentBulletPosition += 1
+        
 # MAGNIFYING GLASS
-
+    def useMG(self,gun):
+        global currentBulletPosition
+        return gun.getChamber()[currentBulletPosition]
 # SAW
+    def useSaw(self, gun):
+        gun.sawed = True
+        sleep(1)
+        print("You sawed off the gun")
+        print("Now it deal 2 damages")
+        print()
 
 # CIGAR
-
+    def useCigar(self):
+        self.hp = self.hp + 1
+        print(f"{self.name} restores 1 hp")
 
 
 
@@ -217,10 +287,10 @@ def bothPlayerAlive(p1,p2):
     if p1.getHP() > 0 and p2.getHP() > 0: return True
     return False
 
-def showHealthBullet(p1,p2,totalBullet, currentBulletPosition):
+def showHealthBullet(p1,p2,totalBullet, currentBulletPosi):
     print(f"\nPLAYER hp: {p1.getHP()}")
     print(f"BOT hp: {p2.getHP()}")
-    print(f"Bullet Remaining: {totalBullet - currentBulletPosition}")
+    print(f"Bullet Remaining: {totalBullet - currentBulletPosi}")
 
 
 
@@ -228,15 +298,15 @@ def showHealthBullet(p1,p2,totalBullet, currentBulletPosition):
 
 # -------------------------------------------ACTUAL GAME HERE------------------------------------------------------------------
 # ini the first shotgun cycle
+currentBulletPosition = 0
 gun = shotgun()
 gun.setChamber()
 currentChamber = gun.getChamber()
-currentBulletPosition = 0
 
 # ini player health 
 health = round(gun.getTotalShot()/2)
-p1 = Player(health)
-p2 = Player(health)
+p1 = Player(health, "PLAYER")
+p2 = Player(health, "BOT")
 
 # announce player hp and gun shots
 print(f"\neach player has {round(gun.getTotalShot()/2)} hp")
@@ -258,21 +328,22 @@ while bothPlayerAlive(p1,p2):
     # ---PLAYER MOVE---
     sleep(1)
     while gun.hasBullet(currentBulletPosition, gun.getTotalShot()) and bothPlayerAlive(p1,p2):
-        while playerTurn == 1 and gun.hasBullet(currentBulletPosition, gun.getTotalShot()):
-            # print(gun.getChamber())
+        while playerTurn == 1 and gun.hasBullet(currentBulletPosition, gun.getTotalShot()) and bothPlayerAlive(p1,p2):
+            print(gun.getChamber())
             print("\n---PLAYER MOVE---")
-            p1.playerMove(currentChamber[currentBulletPosition],p2, 2)
-            showHealthBullet(p1, p2, gun.getTotalShot(), currentBulletPosition)
+
+            p1.playerMove(currentChamber[currentBulletPosition],p2, 2, gun)
+            
         
         if not gun.hasBullet(currentBulletPosition, gun.getTotalShot()):
             break
 
         # ---BOT MOVE---
         while bothPlayerAlive(p1,p2) and playerTurn == 2 and gun.hasBullet(currentBulletPosition, gun.getTotalShot()):
-            # print(gun.getChamber())
+            print(gun.getChamber())
             print("\n---BOT MOVE---")
             p2.botMove(currentChamber[currentBulletPosition], p1, 1, gun)
-            showHealthBullet(p1, p2, gun.getTotalShot(), currentBulletPosition)
+
 
     # --- RELOAD if both player alive---
     if not gun.hasBullet(currentBulletPosition, gun.getTotalShot()) and bothPlayerAlive(p1,p2):
