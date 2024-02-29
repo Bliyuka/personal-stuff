adb3e627676f347ef391f16257c3b0903923db7d
Nguyễn Minh Tuấn <minhtuann903@gmail.com>
2024-02-29 10:43:48 +0700

    improve number layout

--- Python/tic tac toe/tictactoe.py
+++ Python/tic tac toe/tictactoe.py
@@ -11,11 +11,11 @@ gameRunning = True
 # printing the game board 
 def printBoard(board):
     print(" ___________")
-    print("| " + board[0] + " | " + board[1] + " | " + board[2] + " |")
+    print("| " + board[0] + " | " + board[1] + " | " + board[2] + " |" + "  1  2  3")
     print("|---|---|---|")
-    print("| " + board[3] + " | " + board[4] + " | " + board[5] + " |")
+    print("| " + board[3] + " | " + board[4] + " | " + board[5] + " |" + "  4  5  6")
     print("|---|---|---|")
-    print("| " + board[6] + " | " + board[7] + " | " + board[8] + " |")
+    print("| " + board[6] + " | " + board[7] + " | " + board[8] + " |" + "  7  8  9")
 
 # take player input
 def playerInput(board):
