04958b9d1cab0d65b5fbdebb361c543e5c8e499a
Bliyuka <minhtuann903@gmail.com>
2024-08-09 12:43:20 +0700

    text to speech using gTTS from google

--- Python/DiscordBot/project/v2.py
+++ Python/DiscordBot/project/v2.py
@@ -9,8 +9,16 @@ from PIL import Image
 import requests
 from io import BytesIO
 from pyzbar.pyzbar import decode
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+import stat
+import tempfile
 
-
+file_path = 'F:/Coding stuff/personal-stuff/Python/DiscordBot/project'
+os.chmod(file_path, stat.S_IRUSR | stat.S_IWUSR)
+file_path = 'C:/Users/ADMIN/AppData/Local/Temp'
+os.chmod(file_path, stat.S_IRUSR | stat.S_IWUSR)
 
 
 pingSpamActive = True
@@ -89,10 +97,41 @@ async def send_message(message: Message, user_message: str) -> None:
             elif 'mremove' == user_message[:7]: await remove_messages(message, user_message)
 
             # scanning qr code
-            elif 'qr scan' in user_message[:7]: await QR_Scanner(message, user_message)
+            elif 'qr scan' == user_message[:7]: await QR_Scanner(message, user_message)
+
+            elif 'join' in user_message:
+                
+                if message.author.voice:
+                    channel = message.author.voice.channel
+                    try:
+                        vc = await channel.connect()
+                        await message.reply(f"Connected to {channel}")
+                    except discord.errors.ClientException as e:
+                        await message.channel.send(f"Error connecting to voice channel: {e}")
+                else:
+                    await message.reply("Vào voice ngồi đi đã r hẵng gọi t thằng lonz")
+
+            elif 'leave' in user_message:
+                if message.guild.voice_client:
+                    await message.reply('fuck this im out')
+                    await message.guild.voice_client.disconnect()
+                else:
+                    await message.reply(f'I\'m not even in a voice chat bruh')
+
+            elif 'tts' in user_message:
+                text = message.content[len('tts '):]
+                tts = gTTS(text)
+                tts.save('tts.mp3')  
+
+                if message.guild.voice_client:
+                    message.guild.voice_client.play(discord.FFmpegPCMAudio('tts.mp3'), after=lambda e: print('done', e))
+                    # os.remove('tts.mp3')
+                else:
+                    await message.reply('let me in mtfk')
 
             # help, roll dice and other unrelated stuff
             else: await message.reply(get_response(user_message))
+
 
         except Exception as e:
             print(e)
@@ -124,3 +163,21 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
+    
+# async def test(message):
+#     if message.content.startswith('!tts'):
+#             text = message.content[len('!tts '):]
+#             tts = gTTS(text, lang='en')
+#             tts.save("tts.mp3")
+            
+#             voice_client = message.guild.voice_client
+#             if voice_client and voice_client.is_connected():
+#                 audio = AudioSegment.from_mp3("tts.mp3")
+#                 play(audio)
+#                 os.remove("tts.mp3")
+#             else:
+#                 await message.channel.send("I am not connected to a voice channel.")
