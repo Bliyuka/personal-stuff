e41f07547474bbca0b5237354da1b92f017392d0
Bliyuka <minhtuann903@gmail.com>
2024-08-09 10:01:07 +0700

    separate the function for easier look

--- Python/DiscordBot/project/v2.py
+++ Python/DiscordBot/project/v2.py
@@ -25,6 +25,50 @@ intents: Intents = Intents.default()
 intents.message_content = True 
 client: Client= Client(intents=intents)
 
+# --------------------------FUNCTIONS--------------------
+async def ping(ctx: Message, uMess) -> None:
+    global pingSpamActive
+    if 'stop' in uMess:
+        pingSpamActive = False
+        await ctx.reply('ping stopped')
+        print('print stopped')
+    else:
+        user_ping = get_uID(uMess)
+        if user_ping == 'no user found':
+            await ctx.channel.send(user_ping)
+            return
+        try:
+            times = get_times(uMess)
+            if times >100: await ctx.channel.send('No tagging more than 100 times')
+            elif times <1: times = 1
+        except: times = 1
+        
+        for i in range(times):
+            if not pingSpamActive: break
+            sleep(0.5)
+            await ctx.channel.send(f'{i+1}/{times} {user_ping}')
+
+async def remove_messages(ctx:Message, uMess) -> None:
+    try: times = get_times(uMess) + 1
+    except: times = 2
+    await ctx.channel.purge(limit=times)
+
+async def QR_Scanner(ctx: Message, uMess) -> None:
+    try:
+        response = requests.get(ctx.attachments[0].url)
+        img = Image.open(BytesIO(response.content))
+        result = decode(img)
+        url = result[0].data.decode('utf-8')
+        await ctx.reply(url)
+    except Exception as e:
+        await ctx.reply(f'Cannot scan - {e}')
+
+
+
+# -----------------------------------------------------------
+# ------------------------MAIN STUFF HERE--------------------
+# -----------------------------------------------------------
+
 
 # STEP 2: MESSAGE FUNCTIONALITY
 async def send_message(message: Message, user_message: str) -> None:
@@ -38,47 +82,21 @@ async def send_message(message: Message, user_message: str) -> None:
         user_message = user_message[1:]
         try: # <----- shit happen from here
             # ping spam command
-            if 'ping' in user_message:
-                if 'stop' in user_message:
-                    pingSpamActive = False
-                    await message.reply('ping stopped')
-                else:
-                    user_ping = get_uID(user_message)
-                    if user_ping == 'no user found':
-                        await message.channel.send(user_ping)
-                        return
-                    try:
-                        times = get_times(user_message)
-                        if times >100: await message.channel.send('no tagging more than 100 times') 
-                        elif times <1: times = 1
-                    except:
-                        times = 1
-
-                    for i in range(times):
-                        if not pingSpamActive: 
-                            break
-                        await message.channel.send(f'{i+1}/{times} {user_ping}')
+            # print(user_message)
+            if 'ping' == user_message[:4]: await ping(message, user_message)
             
             # mass messages removal
-            elif 'mremove' in user_message:
-                try: times = get_times(user_message) + 1
-                except: times = 2
-                await message.channel.purge(limit=times)
+            elif 'mremove' == user_message[:7]: await remove_messages(message, user_message)
 
             # scanning qr code
-            elif 'qr scan' in user_message:
-                response = requests.get(message.attachments[0].url)
-                img = Image.open(BytesIO(response.content))
-                result = decode(img)
-                url = result[0].data.decode('utf-8')
-                await message.reply(url)
+            elif 'qr scan' in user_message[:7]: await QR_Scanner(message, user_message)
 
             # help, roll dice and other unrelated stuff
-            else:
-                await message.reply(get_response(user_message))
+            else: await message.reply(get_response(user_message))
 
         except Exception as e:
             print(e)
+
 
 # STEP 3: HANDLING THE STARTUP FOR OUR BOT
 @client.event
@@ -97,6 +115,8 @@ async def on_message(message: Message) -> None:
     
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
+
+
 
 # STEP 5: MAIN ENTRY POINT
 def main() -> None:
