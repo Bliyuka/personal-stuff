a1ab8a1d852cc4aebc41a681314fddf32f87a79e
Bliyuka <minhtuann903@gmail.com>
2024-08-12 19:29:35 +0700

    play soundboards using local mp3 files

--- Python/DiscordBot/project/v2.py
+++ Python/DiscordBot/project/v2.py
@@ -13,15 +13,19 @@ from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
 import stat
-import tempfile
+import yt_dlp
 
 file_path = 'F:/Coding stuff/personal-stuff/Python/DiscordBot/project'
 os.chmod(file_path, stat.S_IRUSR | stat.S_IWUSR)
 file_path = 'C:/Users/ADMIN/AppData/Local/Temp'
 os.chmod(file_path, stat.S_IRUSR | stat.S_IWUSR)
 
-
 pingSpamActive = True
+
+
+
+
+
 
 # STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
 load_dotenv()
@@ -32,6 +36,8 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 intents: Intents = Intents.default()
 intents.message_content = True 
 client: Client= Client(intents=intents)
+
+
 
 # --------------------------FUNCTIONS--------------------
 async def ping(message: Message, uMess) -> None:
@@ -75,8 +81,8 @@ async def joinVC(message: Message, uMess) -> None:
     if message.author.voice:
         channel = message.author.voice.channel
         try:
-            await channel.connect()
             await message.reply(f"Connected to {channel}")
+            await channel.connect()
         except discord.errors.ClientException as e:
             await message.channel.send(f"Error connecting to voice channel: {e}")
 
@@ -91,28 +97,57 @@ async def leaveVC(message: Message, uMess) -> None:
         await message.reply(f'I\'m not even in a voice chat bruh')
 
 async def TTS(message: Message, uMess) -> None:
-    text = message.content[len('tts     '):]
-    if message.content[len('tts '):len('tts ')+2] == 'vn': lang = 'vn'
-    else: lang = 'en'
-    tts = gTTS(text, lang=lang)
-    tts.save('tts.mp3')
-
     if not message.guild.voice_client:
         await joinVC(message, uMess)
 
+
+    text = message.content[len('tts '):]
+    # if message.content[len('tts '):len('tts ')+2] == 'vn': lang = 'vn'
+    # else: lang = 'en'
+    tts = gTTS(text)
+    tts.save('tts.mp3')
+
+
     if message.guild.voice_client:
-        message.guild.voice_client.play(discord.FFmpegPCMAudio('tts.mp3'), after=lambda e: print('done', e))
+        message.guild.voice_client.play(discord.FFmpegPCMAudio('tts.mp3'), after=lambda e: print('tts played', e))
         # os.remove('tts.mp3')
     else:
         await message.reply('let me in mtfk')
 
+async def soundboard(message: Message, uMess) -> None:
 
+    if not message.guild.voice_client:
+        await joinVC(message, uMess)
+    
+    # list available soundboards
+    if uMess == 'sb':
+        folder_path = 'F:/Coding stuff/personal-stuff/Python/DiscordBot/project/soundboard'
+        filenames = []
+        for filename in os.listdir(folder_path):
+            file_name_only = filename.split(".")
+            filenames.append(file_name_only[0])
+        
+        filenames_string = " \n".join(filenames)
+        await message.reply(filenames_string)
+    
+    # play the chosen soundboard
+    elif uMess[:len('sb ')] == 'sb ':
+        soundboard_name = message.content[len('tts '):]
+        soundboard_PATH = 'soundboard/' + soundboard_name + '.mp3'
+        
+        if message.guild.voice_client:
+            message.guild.voice_client.play(discord.FFmpegPCMAudio(soundboard_PATH), after=lambda e: print(f'{soundboard_name} played - {e}'))
+            await message.channel.send(f'played {soundboard_name}')
+            await message.channel.purge(limit=2)
+    
+    # chosen soundboard not available
+    else:
+        await message.reply('Soundboard not available')
 
 
 # -----------------------------------------------------------
 # ------------------------MAIN STUFF HERE--------------------
 # -----------------------------------------------------------
-
 
 # STEP 2: MESSAGE FUNCTIONALITY
 async def send_message(message: Message, user_message: str) -> None:
@@ -144,18 +179,19 @@ async def send_message(message: Message, user_message: str) -> None:
             # text to speech
             elif 'tts' == user_message[:len('tts')]: await TTS(message, user_message)
 
+            elif 'sb' == user_message[:len('sb')]: await soundboard(message, user_message)
+
             # help, roll dice and other unrelated stuff
             else: await message.reply(get_response(user_message))
 
-
-        except Exception as e:
-            print(e)
+        except Exception as e: print(e)
 
 
 # STEP 3: HANDLING THE STARTUP FOR OUR BOT
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
+
 
 # STEP 4: HANDLING INCOMING MESSAGES
 @client.event
@@ -169,7 +205,6 @@ async def on_message(message: Message) -> None:
     
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
-
 
 
 # STEP 5: MAIN ENTRY POINT
