dd9e04109352568e16f4d8348d3dd02301fbd486
Bliyuka <minhtuann903@gmail.com>
2024-08-18 21:15:17 +0700

    tried to do youtube music play but eh, so leave it to v3 with additional libs

--- Python/DiscordBot/project/v2.py
+++ Python/DiscordBot/project/v2.py
@@ -2,18 +2,11 @@ from typing import Final
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-import discord
-from responses import get_response, get_uID, get_times
-from time import sleep
-from PIL import Image
-import requests
-from io import BytesIO
-from pyzbar.pyzbar import decode
-from gtts import gTTS
-from pydub import AudioSegment
-from pydub.playback import play
+from functions import *
 import stat
 import yt_dlp
+import discord
+from time import sleep
 
 file_path = 'F:/Coding stuff/personal-stuff/Python/DiscordBot/project'
 os.chmod(file_path, stat.S_IRUSR | stat.S_IWUSR)
@@ -21,10 +14,8 @@ file_path = 'C:/Users/ADMIN/AppData/Local/Temp'
 os.chmod(file_path, stat.S_IRUSR | stat.S_IWUSR)
 
 pingSpamActive = True
-
-
-
-
+mURL = []
+mTITLE = []
 
 
 # STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
@@ -36,114 +27,6 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 intents: Intents = Intents.default()
 intents.message_content = True 
 client: Client= Client(intents=intents)
-
-
-
-# --------------------------FUNCTIONS--------------------
-async def ping(message: Message, uMess) -> None:
-    global pingSpamActive
-    if 'stop' in uMess:
-        pingSpamActive = False
-        await message.reply('ping stopped')
-        print('print stopped')
-    else:
-        user_ping = get_uID(uMess)
-        if user_ping == 'no user found':
-            await message.channel.send(user_ping)
-            return
-        try:
-            times = get_times(uMess)
-            if times >100: await message.channel.send('No tagging more than 100 times')
-            elif times <1: times = 1
-        except: times = 1
-        
-        for i in range(times):
-            if not pingSpamActive: break
-            sleep(0.5)
-            await message.channel.send(f'{i+1}/{times} {user_ping}')
-
-async def remove_messages(message:Message, uMess) -> None:
-    try: times = get_times(uMess) + 1
-    except: times = 2
-    await message.channel.purge(limit=times)
-
-async def QR_Scanner(message: Message, uMess) -> None:
-    try:
-        response = requests.get(message.attachments[0].url)
-        img = Image.open(BytesIO(response.content))
-        result = decode(img)
-        url = result[0].data.decode('utf-8')
-        await message.reply(url)
-    except Exception as e:
-        await message.reply(f'Cannot scan - {e}')
-
-async def joinVC(message: Message, uMess) -> None:
-    if message.author.voice:
-        channel = message.author.voice.channel
-        try:
-            await message.reply(f"Connected to {channel}")
-            await channel.connect()
-        except discord.errors.ClientException as e:
-            await message.channel.send(f"Error connecting to voice channel: {e}")
-
-    else:
-        await message.reply("Vào voice ngồi đi đã r hẵng gọi t thằng lonz")
-
-async def leaveVC(message: Message, uMess) -> None:
-    if message.guild.voice_client:
-        await message.reply('fuck this im out')
-        await message.guild.voice_client.disconnect()
-    else:
-        await message.reply(f'I\'m not even in a voice chat bruh')
-
-async def TTS(message: Message, uMess) -> None:
-    if not message.guild.voice_client:
-        await joinVC(message, uMess)
-
-
-    text = message.content[len('tts '):]
-    # if message.content[len('tts '):len('tts ')+2] == 'vn': lang = 'vn'
-    # else: lang = 'en'
-    tts = gTTS(text)
-    tts.save('tts.mp3')
-
-
-    if message.guild.voice_client:
-        message.guild.voice_client.play(discord.FFmpegPCMAudio('tts.mp3'), after=lambda e: print('tts played', e))
-        # os.remove('tts.mp3')
-    else:
-        await message.reply('let me in mtfk')
-
-async def soundboard(message: Message, uMess) -> None:
-
-    if not message.guild.voice_client:
-        await joinVC(message, uMess)
-    
-    # list available soundboards
-    if uMess == 'sb':
-        folder_path = 'F:/Coding stuff/personal-stuff/Python/DiscordBot/project/soundboard'
-        filenames = []
-        for filename in os.listdir(folder_path):
-            file_name_only = filename.split(".")
-            filenames.append(file_name_only[0])
-        
-        filenames_string = " \n".join(filenames)
-        await message.reply(filenames_string)
-    
-    # play the chosen soundboard
-    elif uMess[:len('sb ')] == 'sb ':
-        soundboard_name = message.content[len('tts '):]
-        soundboard_PATH = 'soundboard/' + soundboard_name + '.mp3'
-        
-        if message.guild.voice_client:
-            message.guild.voice_client.play(discord.FFmpegPCMAudio(soundboard_PATH), after=lambda e: print(f'{soundboard_name} played - {e}'))
-            await message.channel.send(f'played {soundboard_name}')
-            await message.channel.purge(limit=2)
-    
-    # chosen soundboard not available
-    else:
-        await message.reply('Soundboard not available')
-
 
 # -----------------------------------------------------------
 # ------------------------MAIN STUFF HERE--------------------
@@ -161,8 +44,27 @@ async def send_message(message: Message, user_message: str) -> None:
         user_message = user_message[1:]
         try: # <----- shit happen from here
             # ping spam command
-            # print(user_message)
-            if 'ping' == user_message[:len('ping')]: await ping(message, user_message)
+            if 'ping' == user_message[:len('ping')]: 
+                # await ping(message, user_message, pingSpamActive)
+                if 'stop' in user_message:
+                    pingSpamActive = False
+                    await message.reply('ping stopped')
+                    print('print stopped')
+                else:
+                    user_ping = get_uID(user_message)
+                    if user_ping == 'no user found':
+                        await message.channel.send(user_ping)
+                        return
+                    try:
+                        times = get_times(user_message)
+                        if times >100: await message.channel.send('No tagging more than 100 times')
+                        elif times <1: times = 1
+                    except: times = 1
+                    
+                    for i in range(times):
+                        if not pingSpamActive: break
+                        sleep(0.5)
+                        await message.channel.send(f'{i+1}/{times} {user_ping}')
 
             # mass messages removal
             elif 'mremove' == user_message[:len('mremove')]: await remove_messages(message, user_message)
@@ -179,19 +81,122 @@ async def send_message(message: Message, user_message: str) -> None:
             # text to speech
             elif 'tts' == user_message[:len('tts')]: await TTS(message, user_message)
 
+            # soundboard
             elif 'sb' == user_message[:len('sb')]: await soundboard(message, user_message)
+
+            # choosing out 1 given options
+            elif 'choose' == user_message[:len('choose')]: await choose(message, user_message)
+
+
+            # elif 'p' == user_message[:len('p')]:
+            #     global mURL
+            #     global mTITLE
+                
+            #     options = {
+            #         'format': 'bestaudio/best',
+            #         'keepvideo': False,
+            #         'outtmpl': 'song.mp3',
+            #         'extract_flat': True,
+            #         'quiet': True,
+            #     }
+                
+            #     # join vc if not in yet
+            #     if not message.guild.voice_client:
+            #         await joinVC(message, user_message)
+
+            #     # search using url
+            #     if 'http' in user_message:
+            #         video_url = message.content.split(' ')[1]
+
+            #     # search with query
+            #     else:
+            #         query = message.content[len('p '):]
+            #         with yt_dlp.YoutubeDL(options) as ydl:
+            #             video_info = ydl.extract_info(f"ytsearch:{query}", download=False)
+            #             if 'entries' in video_info:
+            #                 video_url = video_info['entries'][0]['url']
+
+            #     print('url extracted')
+
+            #     video_info = yt_dlp.YoutubeDL().extract_info(url=video_url, download=False)
+            #         # filename = f"{video_info['title']}.mp3"
+
+            #     print('info extracted') # <-----------------
+            #     print('tao')
+            #     url = video_info['webpage_url']
+            #     print('xin')
+            #     mURL.append(url)
+            #     print('may')
+            #     title = video_info['title']
+            #     print('wtf')
+            #     mTITLE.append(title)
+            #     print('add đi')
+                
+            #     await message.channel.send(f'Added **{title}**')
+            #     # video_info = mURL[0]
+            #     # mURL.pop(0)
+
+            #     print('list added')
+            #     voice_client = message.guild.voice_client
+            #     if not voice_client.is_playing():
+            #         while mURL:
+            #             print('in while loop')
+            #             try: os.remove('song.mp3')
+            #             except Exception as e: print(e) 
+            #             print('tried remove song.mp3')
+                        
+            #             with yt_dlp.YoutubeDL(options) as ydl:
+            #                 ydl.download([mURL[0]])
+                            
+            #                 async def after_playing(error):
+            #                     if error: print(f'ERROR: {error}')
+            #                     if message.guild.voice_client:
+            #                         print('play song')
+            #                         mURL.pop(0)
+            #                         mTITLE.pop(0)
+            #                         await message.channel.send(f'Now playing - **{mTITLE[0]}**')
+            #                         message.guild.voice_client.play(discord.FFmpegPCMAudio('song.mp3'), after=after_playing)
+            #             # print(f"Download complete... {filename}")
+
+            #             print('download next song')
+
+            #             if message.guild.voice_client:
+            #                 print('play song')
+            #                 await message.channel.send(f'Now playing - **{mTITLE[0]}**')
+            #                 message.guild.voice_client.play(discord.FFmpegPCMAudio('song.mp3'), after= after_playing)
+                            
+
+
+
+
+
+# TODO:
+# 1. pause
+# 2. resume / play
+# 3. queue
+# 4. skip / stop (DONE)
+# 5. search for song (DONE)
+
+
+            elif 's' == user_message[:len('s')]:
+                voice_client = message.guild.voice_client
+                print('stop song')
+                if voice_client.is_playing():
+                    voice_client.stop()
 
             # help, roll dice and other unrelated stuff
             else: await message.reply(get_response(user_message))
 
         except Exception as e: print(e)
 
+# -----------------------------------------------------------
+# ------------------------MAIN STUFF ABOVE-------------------
+# -----------------------------------------------------------
 
 # STEP 3: HANDLING THE STARTUP FOR OUR BOT
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
-
 
 # STEP 4: HANDLING INCOMING MESSAGES
 @client.event
@@ -206,10 +211,11 @@ async def on_message(message: Message) -> None:
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
-
 # STEP 5: MAIN ENTRY POINT
 def main() -> None:
     client.run(token=TOKEN)
 
 if __name__ == '__main__':
     main()
+
+
